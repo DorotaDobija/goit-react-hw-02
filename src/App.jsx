ad52134be49398ba89cb87338fc5a72b5afe4c4e
Dorota Dobija <117933735+DorotaDobija@users.noreply.github.com>
2024-08-21 22:21:58 +0200

    useEffect and working with localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Description } from './Description/Description'
 import { Options } from './Options/Options'
@@ -7,15 +7,33 @@ import { Notification } from './Notification/Notification'
 
 export const App = () => {
 
+  const [rate, setRate] = useState(() => {
 
-  const [rate, setRate] = useState({
+    const savedRate = window.localStorage.getItem("saved-rate");
+
+    if (savedRate !== null) {
+      return JSON.parse(savedRate)
+    }
+
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0
+    }
+  })
+	
+  useEffect(() => {
+    window.localStorage.setItem("saved-rate", JSON.stringify(rate))
+  });
+
+  const clearState = () =>
+    setRate({
 	good: 0,
 	neutral: 0,
 	bad: 0
   })
 
   const updateFeedback = (feedbackType) => {
-
     setRate((prevRate) => ({
       ...prevRate,
       [feedbackType]: prevRate[feedbackType] + 1,
@@ -30,10 +48,13 @@ export const App = () => {
     <Options variant="good" updateRate={updateFeedback}>Good</Options>
     <Options variant="neutral" updateRate={updateFeedback}>Neutral</Options>
     <Options variant="bad" updateRate={updateFeedback}>Bad</Options>
+    {totalFeedback > 0 && <Options updateRate={clearState}>Reset</Options>}
     {totalFeedback > 0 ? (<>
     <Feedback>Good: {rate.good}</Feedback>
     <Feedback>Neutral: {rate.neutral }</Feedback>
     <Feedback>Bad: {rate.bad}</Feedback>
+    <Feedback>Total: {totalFeedback}</Feedback>
+    <Feedback>Positive: {Math.round(((rate.good + rate.neutral) / totalFeedback) * 100)}%</Feedback>
     </>) : (<Notification />)}
   </>
 }
